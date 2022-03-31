3f86d009b402190f6a54220b35e76c3bdc29cbed
jairuscalvin <jairus.calvin@gmailcom>
2022-03-31 10:35:09 +0700

    Tugas 1

--- 3. CoreModules/lat.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-fs.readFile('Learn To Code.txt','utf-8', (err,data) => {
-    if (err) throw err;
-    console.log(data)
-});
-console.log(fs);
--- 3. NPM/app.js
+++
@@ -1,4 +0,0 @@
-const validator = require('validator');
-
-// console.log(validator.isEmail('adit@gmail.co.id'));
-console.log(validator.isMobilePhone('087214135963','id-ID'));
