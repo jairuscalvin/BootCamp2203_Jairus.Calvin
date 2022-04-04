a0e9431d326a2c2a3df52616b1900f1b06fa453a
jairuscalvin <jairus.calvin@gmailcom>
2022-04-04 13:26:59 +0700

    Contact App 2 tugas pertama

---
+++ 11. Contact App 2/app.js
@@ -0,0 +1,78 @@
+const express = require('express')
+var expressLayouts = require('express-ejs-layouts')
+const { url } = require('inspector')
+const path = require('path')
+const fs = require('fs')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+//pengecekan file json
+const dirPath='./data';
+if(!fs.existsSync(dirPath)){
+    fs.mkdirSync(dirPath);
+}
+const dataPath = './data/contacts.json';
+if(!fs.existsSync(dataPath)){
+    fs.writeFileSync(dataPath,'[]','utf-8');
+}
+
+//membaca file json
+const loadContact = () => {
+    const file = fs.readFileSync('data/contacts.json','utf-8');
+    const contacts = JSON.parse(file);
+    return contacts;
+}
+
+//instalation EJS dengan views
+app.set('view engine','ejs') 
+app.use(expressLayouts)
+app.set("layout extractScripts", true)
+app.use(morgan('dev'))
+
+//memberikan folder styles menjadi statis
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    console.log('Time:', Date.now())
+    next()
+  })
+
+//routes
+app.get('/', (req, res) => {
+    // res.sendFile(__dirname + '/index.html')
+    const nama = 'Jairus Calvin'
+    let index = 1
+    const kontak = loadContact();
+    res.render('index', {
+        title: 'Home',
+        nama,
+        index,
+        kontak,
+        layout: 'layouts/main-layouts',
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        layout: 'layouts/main-layouts',
+    })
+})
+app.get('/contact', (req, res) => {
+    res.render('contact', {
+        title: 'Contact',
+        layout: 'layouts/main-layouts',
+    })
+})
+app.get('/product/:product_id', (req, res) => {
+    res.send('product id : ' + req.params.product_id + '<br> category id : ' + req.query.category)
+})
+
+app.use('/', (req, res) => {
+    res.status(404);
+    res.send('<h2>404 - Page Not Found!!</h2>');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
