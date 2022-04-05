d1c03f656103e5bef046b77990a89ce784df9d9a
jairuscalvin <jairus.calvin@gmailcom>
2022-04-05 11:01:00 +0700

    Contacts App hari kedua

---
+++ 12. Contact App 3/data/contacs.js
@@ -0,0 +1,1 @@
+[{"name":"Jairus","phone":"082248987654","email":"jairus.calvin@gmail.com"},{"name":"Calvin","phone":"0812455587654","email":"calvin@gmail.com"},{"name":"Mei","phone":"0812455589999","email":"mei@gmail.com"}]
